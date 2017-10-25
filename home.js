3d187c33b274b459d6f6fbcad807209e66603b0e
NRothera <31472463+NRothera@users.noreply.github.com>
2017-10-25 18:36:31 +0100

    button positioning

--- home.js
+++ home.js
@@ -1,7 +1,10 @@
 
-var number_generator = Math.floor(Math.random() * 3)
-var quotes = ["Never give up on your dreams", "Good things take time", "You are stronger than you think"]
+
+var quotes = ["Never give up on your dreams...", "Good things take time...", "You are stronger than you think..."]
 
 var change_text = function(){
-  document.getElementById('quotes').innerHTML = 'Hello';
+  for (var i = 0; i < 30; i++) {
+      var number_generator = Math.floor(Math.random() * 3)
+      document.getElementById('quotes').innerHTML = quotes[number_generator];
+  }
 }
