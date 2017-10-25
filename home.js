5cdee9aacbeb9997b6a376eb2f321c2f70f236bc
NRothera <31472463+NRothera@users.noreply.github.com>
2017-10-25 20:53:17 +0100

    changed text

--- home.js
+++ home.js
@@ -1,10 +1,11 @@
 
 
-var quotes = ["Never give up on your dreams...", "Good things take time...", "You are stronger than you think..."]
+var quotes = ["A dreamer, probably one of the greatest minds of our generation, Nicolas encompasses all the qualities of a genius.", "A person who is extremely humble, Nicolas will do what needs to be done better and faster than everyone else", "Hire him now while he is still undiscovered! In a few years time you will be proud to be able to say you had a man as amazing as Nicolas on your team"]
 
 var change_text = function(){
-  for (var i = 0; i < 30; i++) {
+  for (var i = 0; i < 4; i++) {
       var number_generator = Math.floor(Math.random() * 3)
       document.getElementById('quotes').innerHTML = quotes[number_generator];
+
   }
 }
