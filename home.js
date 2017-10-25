5c66bd5e89149f9ef56f16ae12f11f1eb30bd007
NRothera <31472463+NRothera@users.noreply.github.com>
2017-10-25 17:50:13 +0100

    added javascript

---
+++ home.js
@@ -0,0 +1,7 @@
+
+var number_generator = Math.floor(Math.random() * 3)
+var quotes = ["Never give up on your dreams", "Good things take time", "You are stronger than you think"]
+
+var change_text = function(){
+  document.getElementById('quotes').innerHTML = 'Hello';
+}
